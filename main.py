15cd46dcef1cee7699d5a6cb5883effd9dc89564
Rebecca J Proctor <jproctor.m.ed.tn@gmail.com>
2020-12-23 22:31:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#strings
+'Hi, Hello'
+"How are you?"
+print(type('12, how are you?'))
+
+user_name = 'supercoder'
+password ='supersecret'
+long_string = '''
+WOW
+0 0
+___ 
+'''
+print(long_string)
+
+first_name = 'Jeannine'
+last_name = 'Proctor'
+full_name = first_name + ' ' + last_name
+print(full_name)
